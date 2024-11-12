aa3118388675411fe60df766e788dd97a4e02a4c
ahron <ahroni559@gmail.com>
2024-11-12 10:58:41 +0200

    cvvcv

---
+++ 45.py
@@ -0,0 +1,35 @@
+#  class person():
+#      def __init__(self):
+#         self.names = []
+#          self.lasts = []
+#          self.ages = []
+#      def  __add__(self,name,last,age):
+#
+#         self.names.append(name)
+#         self.lasts.append(last)
+#         self.ages.append(age)
+#         if age >=18:
+#             print("good")
+#         else:
+#             print("not good")
+#  class student()
+#      def
+# self.names.append(name)
+#
+#
+# class prson():
+#     def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
